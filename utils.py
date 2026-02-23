e87e2046480946d0a30eed36f7eab27178e3ba36
EorlEli <51798919+EorlEli@users.noreply.github.com>
2026-02-23 09:49:14 +0100

    Update Nova-3 pricing

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ PRICING = {
     # Cost per 1 Million Tokens
     "gpt-5-nano":  {"input": 0.05, "output": 0.40},
     "gpt-5.2":  {"input": 1.75, "output": 14.00},
-    "whisper-1":   0.0043  # Cost per MINUTE of audio
+    "whisper-1":   0.0065 #0.0043  # Cost per MINUTE of audio
 }
 
 # Global accumulation variable
